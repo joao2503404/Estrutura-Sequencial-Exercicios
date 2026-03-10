c58bb96fbfbc120bdafb0869904bafd6b8bad5f1
joaovieira0087 <joaovieira0087@gmail.com>
2026-03-10 20:13:15 -0300

    exercício 03 sexo do usuario

---
+++ 01Lista-de-Exercicios-EstruturadeDecisao/exe03.py
@@ -0,0 +1,9 @@
+sexo = (input("Digite o sexo (M/F): ")).upper()
+
+if sexo == "M":
+    print("Masculino")
+elif sexo == "F":
+    print("Feminino")
+else:
+    print("Sexo inválido Digite M ou F")
+    
