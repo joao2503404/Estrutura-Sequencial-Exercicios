f6f651c8da04711e4168c145a9ac6ac9dc0f8d0f
joaovieira0087 <joaovieira0087@gmail.com>
2026-03-10 19:33:44 -0300

    exe 01 maior numero

---
+++ 01Lista-de-Exercicios-EstruturadeDecisao/exe01.py
@@ -0,0 +1,7 @@
+numero01 = int(input("Digite o primeiro número:"))
+numero02 = int(input("Digite o segundo número:"))
+
+if numero01 > numero02:
+    print("O primeiro número é maior que o segundo número.")
+else:
+    print("O segundo número é maior que o primeiro número.")
