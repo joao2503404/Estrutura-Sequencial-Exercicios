72c3160e5aea8d3a4e33fac0d5b5d2a2ac65959a
joaovieira0087 <joaovieira0087@gmail.com>
2026-03-10 14:50:49 -0300

    exercicio 4 media

---
+++ EstruturaSequencial-Exercicios/exe04.py
@@ -0,0 +1,10 @@
+
+nota = float(input("Digite a primeira nota: "))
+nota1 = float(input("Digite a segunda nota: "))
+nota2 = float(input("Digite a terceira nota: "))
+nota3 = float(input("Digite a quarta nota: "))
+
+
+media = (nota + nota1 + nota2 + nota3) / 4
+
+print("A média do aluno é: ", media)
