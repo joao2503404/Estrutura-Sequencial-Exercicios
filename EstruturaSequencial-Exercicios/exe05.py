53c4dcba5ec5eb13d094e98ccf18be27617c62a3
joaovieira0087 <joaovieira0087@gmail.com>
2026-03-10 15:02:50 -0300

    exercício 5 converta metros para centímetros

---
+++ EstruturaSequencial-Exercicios/exe05.py
@@ -0,0 +1,6 @@
+mensagem = "converta metros para centímetros"
+print(mensagem.center(50, "-"))
+
+metros = float(input("Digite a quantidade de metros: "))
+centimetros = metros * 100
+print(f"{metros} metros equivalem a {centimetros} centímetros.")
