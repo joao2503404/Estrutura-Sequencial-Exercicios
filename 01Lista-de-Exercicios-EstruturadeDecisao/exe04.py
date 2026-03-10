29564c6c69232e30354c50fba7e36756541dfcef
joaovieira0087 <joaovieira0087@gmail.com>
2026-03-10 20:31:59 -0300

    exercício 04 vogal e consoante

---
+++ 01Lista-de-Exercicios-EstruturadeDecisao/exe04.py
@@ -0,0 +1,6 @@
+mensagem = input("Digite a letra:".ToUpper())
+
+if mensagem == "A" and mensagem != "E" and mensagem != "I" and mensagem != "O" and mensagem != "U":
+    print("A letra digitada é uma consoante")
+else:
+    print("A letra digitada é uma vogal")
