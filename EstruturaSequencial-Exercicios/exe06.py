c1de91d95fda739eccf50ce0a45b96b950cfb5fc
joaovieira0087 <joaovieira0087@gmail.com>
2026-03-10 15:13:28 -0300

    exercício 6 área do circulo

---
+++ EstruturaSequencial-Exercicios/exe06.py
@@ -0,0 +1,8 @@
+mensagem = "área do circulo"
+print(mensagem.center(50, "-"))
+
+raio = float(input("Digite o raio do circulo:"))
+
+area = 3.14 * (raio ** 2)
+
+print(f"A area do circulo é: {area:.2f}")
