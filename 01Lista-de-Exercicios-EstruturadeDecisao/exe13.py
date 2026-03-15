2a78ac54def10d7e73ab6d0f36eb71f019cde9b7
joaovieira0087 <joaovieira0087@gmail.com>
2026-03-15 14:54:32 -0300

    dia da semana

---
+++ 01Lista-de-Exercicios-EstruturadeDecisao/exe13.py
@@ -0,0 +1,20 @@
+valor = int(input("digite qual dia da semana ? "))
+
+if valor == 1:
+    print("Domingo")
+elif valor == 2:
+    print("Segunda Feira")
+elif valor == 3:
+    print("Terça Feira")
+elif valor == 4:
+    print("Quarta Feira")
+elif valor == 5:
+    print("Quinta Feira")
+elif valor == 6:
+    print("Sexta Feira")
+elif valor == 7:
+    print("Sábado")
+else:
+    print("Digite um valor valido")
+
+
