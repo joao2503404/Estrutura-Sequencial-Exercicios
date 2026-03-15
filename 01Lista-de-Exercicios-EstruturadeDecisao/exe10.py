6f48e989d4775aff7d89bbc88b2cd7a6971335ce
joaovieira0087 <joaovieira0087@gmail.com>
2026-03-15 13:32:23 -0300

    Turnos escola

---
+++ 01Lista-de-Exercicios-EstruturadeDecisao/exe10.py
@@ -0,0 +1,12 @@
+valor = input("digite qual turno você estuda ? ").upper()
+
+if valor == 'M':
+    print("Bom Dia")
+
+elif valor == 'V':
+    print("Boa Tarde")
+
+elif valor == 'N' :
+    print("Boa Noite")
+
+
